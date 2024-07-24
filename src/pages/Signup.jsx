d7a9f86f1df9302a0f43130fd69b17ce2b16a196
JimmyLam9758 <JimmyLam9758@gmail.com>
2024-07-25 00:17:15 +1000

    feat: folder structure & login form

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,14 @@
+import LoginForm from '../components/LoginForm.jsx'
+
+
+export default function SignupPage(){
+
+    return (<>
+    <h1>Login / Register page</h1>
+    
+    <LoginForm />
+
+    
+    </>);
+    
+}
