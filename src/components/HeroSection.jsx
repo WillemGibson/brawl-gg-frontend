5cc136fe97f79b1e195cd7054980cf9f36216bd9
Willem Gibson <willemjackgibson@gmail.com>
2024-07-27 12:58:50 +1000

    feat: navigation buttons finalized

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import Demo from '../assets/demo.png'
 
 export default function HeroSection() {
@@ -8,12 +9,12 @@ export default function HeroSection() {
                     Brawlz.gg
                 </h1>
                 <div className='mx-auto mt-5 flex max-w-fit space-x-4'>
-                    <a 
-                        href="#getstarted"
+                    <Link
+                        to="#signup"
                         className="rounded-md mx-auto max-w-fit px-5 py-2 text-base font-medium shadow-sm bg-highlight text-white cursor-pointer hover:bg-amber-500 active:bg-amber-400"
                         >
                             Get Started
-                    </a>
+                    </Link>
                 </div>  
             </div>
             <div className='absolute m-auto left-0 right-0 top-48 bottom-0 z-0'>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Logo from "../assets/logo-transparent.svg";
 
 export default function NavBar() {
@@ -13,18 +14,18 @@ export default function NavBar() {
                             />
                         </a>
                     <div>
-                        <a 
-                            href="#login"
+                        <Link 
+                            to="#login"
                             className="mr-5 max-w-fit px-5 py-2 rounded-md text-white font-bold cursor-pointer relative hover:bg-stone-950 active:bg-zinc-950"
                             >
                                 LOG IN
-                        </a>
-                        <a 
-                            href="#signup"
+                        </Link>
+                        <Link
+                            to="#signup"
                             className="mx-auto max-w-fit px-5 py-2 rounded-md bg-highlight text-white font-bold cursor-pointer relative hover:bg-amber-500 active:bg-amber-400"
                             >
                                 JOIN NOW
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </div>
