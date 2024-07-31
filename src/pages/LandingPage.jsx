fc1f0f39d294cd149c6775070f722cc9e0a45e75
Willem Gibson <willemjackgibson@gmail.com>
2024-07-31 16:41:03 +1000

    fix: fixed overflow issue again

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -8,20 +8,22 @@ import BottomSection from '../components/BottomSection'
 
 export default function LandingPage() {
     return <>
-            <div className='bg-black overflow-hidden'>
-                <div className='w-screen min-h-screen fixed z-10 flex justify-center px-6 py-40 pointer-events-none'>
+            <div className='bg-black'>
+                <div className='w-screen min-h-screen fixed z-10 flex justify-center px-6 py-40 pointer-events-none overflow-auto'>
                     <div className="absolute inset-0 bg-[url('/grid.svg')] opacity-5"></div>
                     <div className='bg-gradient-to-c from-transparent via-transparent to-black to-70% absolute inset-0 z-20'></div>
                 </div>
                 <div className='relative z-20'>
                     <NavBar />
-                    <div className="container mx-auto">
-                        <HeroSection />
-                        <Slider images={partnersLogo} />
-                        <FeaturesSection />
-                        <BottomSection />
+                    <div className='overflow-hidden'>
+                        <div className="container mx-auto">
+                            <HeroSection />
+                            <Slider images={partnersLogo} />
+                            <FeaturesSection />
+                            <BottomSection />
+                        </div>
+                        <Footer />
                     </div>
-                    <Footer />
                 </div>
             </div>
         </>
