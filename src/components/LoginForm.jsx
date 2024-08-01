569fef4603ffbcb9e7c935e294d233c006d9f665
Willem Gibson <willemjackgibson@gmail.com>
2024-08-01 15:32:17 +1000

    fix: merge issues

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -11,18 +11,25 @@ const LoginForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
+    <form className='flex flex-col justify-evenly bg-amber-400 content-center h-full w-1/2 text-center' onSubmit={handleSubmit}>
+      <h4 className='text-black font-bold text-xl'>Login to account</h4>
+
+      <label className='mx-auto text-black text-left text-lg font-bold'>
         Username:
-        <input type="text" value={username} onChange={(event) => setUsername(event.target.value)}/>
+        <input className='text-black rounded border-black border-2 w-full' size="100" type="text" value={username} onChange={(event) => setUsername(event.target.value)}/>
       </label>
-      <br />
-      <label>
-        Password:
-        <input type="password" value={password} onChange={(event) => setPassword(event.target.value)}/>
+
+      <label className='mx-auto text-black text-left text-lg font-bold'>
+        Password: 
+        <input className='text-black rounded border-black border-2 w-full' size="100" type="password" value={password} onChange={(event) => setPassword(event.target.value)}/>
       </label>
-      <br />
-      <button type="submit">Login</button>
+
+      <div className="flex justify-around">
+      <a href="/" className="text-violet-900 font-bold text-l">Forgot password?</a>
+      <a href="/signup" className="text-violet-900 font-bold text-l">Create account</a>
+      </div>
+
+      <button className='mx-auto w-fit px-5 py-2 rounded-md text-white font-bold relative bg-black' type="submit">Login</button>
     </form>
   );
 };
