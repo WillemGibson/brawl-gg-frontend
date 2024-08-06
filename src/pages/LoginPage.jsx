444807e156ec1e09754b2a1bba02a0b215f149b9
Willem Gibson <willemjackgibson@gmail.com>
2024-08-07 09:58:22 +1000

    fix: slight issues

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -7,12 +7,12 @@ export default function LoginPage() {
         <>
         <div className='bg-black'>
             <NavBar />
-            <div className='bg-black'>
+            <div className='bg-black z'>
                 <h2 className="text-center text-white text-3xl font-extrabold m-6 leading-[1.15] sm:text-5xl">
                     Login 
                 </h2>
                 <div className='h-dvh flex flex-col items-center'>
-                    <div class='formcontainer' className='h-2/5 w-1/2 my-10 flex-col content-center flex items-center bg-amber-400 text-center rounded border-white border-2'>
+                    <div className='formcontainer h-2/5 w-1/2 my-10 flex-col content-center flex items-center bg-amber-400 text-center rounded border-white border-2'>
                         <LoginForm/>
                     </div>
                 </div>
